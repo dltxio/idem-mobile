0dc97a40fd639c4b5ec5f84b0d31936243baabaf
Anthony Hinchliff <anthony.hinchliff@gmail.com>
2022-04-19 09:14:59 +1000

    show user details in header

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -2,5 +2,12 @@ import DocumentList from "./DocumentList";
 import ClaimsList from "./ClaimsList";
 import Modal from "./Modal";
 import RequestClaimsModal from "./RequestClaimsModal";
+import UserDetailsHeader from "./UserDetailsHeader";
 
-export { DocumentList, ClaimsList, Modal, RequestClaimsModal };
+export {
+  DocumentList,
+  ClaimsList,
+  Modal,
+  RequestClaimsModal,
+  UserDetailsHeader
+};
