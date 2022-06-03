8fffa14de4c6116cee5b4a9f87b49ed1a9e17d13
simon.arango <simon.arango@betmakers.com>
2022-06-03 12:55:12 +1000

    Remove unused const and components

--- app/src/navigation/screens/VendorsScreen.tsx
+++ app/src/navigation/screens/VendorsScreen.tsx
@@ -1,40 +1,8 @@
 import * as React from "react";
-import { View, Alert } from "react-native";
-import { Button } from "../../components";
-import { useClaimsStore } from "../../context/ClaimsStore";
-import { useDocumentStore } from "../../context/DocumentStore";
+import { View } from "react-native";
 import commonStyles from "../../styles/styles";
 
 const VendorsScreen: React.FC = () => {
-  const { reset: resetDocuments } = useDocumentStore();
-  const { reset: resetClaims } = useClaimsStore();
-
-  const showAlert = () =>
-    Alert.alert(
-      "CAUTION",
-      "You are about to reset your data, including claims and files. Would you like to continue?",
-      [
-        {
-          text: "OK",
-          onPress: onReset,
-          style: "destructive"
-        },
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-        }
-      ],
-      {
-        cancelable: true
-      }
-    );
-
-  const onReset = () => {
-    resetDocuments();
-    resetClaims();
-  };
-
   return (
     <View style={commonStyles.screen}></View>
   );
