9efb9dda9a75f924a238b30319f9f80966ce8d2f
Jo Rocca <loluvulol@gmail.com>
2022-06-06 12:23:43 +1000

    Create third parties list

--- app/src/navigation/screens/VendorsScreen.tsx
+++ app/src/navigation/screens/VendorsScreen.tsx
@@ -1,9 +1,53 @@
 import * as React from "react";
 import { View } from "react-native";
-import commonStyles from "../../styles/styles";
+import axios from "axios";
+import { Vendor } from "../../types/general";
+import { ListItem } from "react-native-elements";
+
+type GetVendorsResponse = {
+  data: Vendor[];
+};
 
 const VendorsScreen: React.FC = () => {
-  return <View style={commonStyles.screen}></View>;
+  const [vendors, setVendors] = React.useState<Vendor[]>([]);
+
+  const getVendors = async () => {
+    try {
+      const { data: vendors }: GetVendorsResponse = await axios.get(
+        "https://raw.githubusercontent.com/dltxio/idem-mobile/IDEM-35/data/sites.json"
+      );
+      setVendors(vendors);
+    } catch (error) {
+      const err = error as any;
+      console.error(err?.response?.data || error);
+    }
+  };
+
+  React.useLayoutEffect(() => {
+    getVendors();
+  }, []);
+
+  return (
+    <View>
+      {vendors.map((vendor) => {
+        const vendorList = getVendors();
+
+        if (!vendorList) {
+          return null;
+        }
+
+        const content = (
+          <>
+            <ListItem.Content>
+              <ListItem.Title>{vendor.name}</ListItem.Title>
+            </ListItem.Content>
+          </>
+        );
+
+        return <ListItem key={vendor.website}>{content}</ListItem>;
+      })}
+    </View>
+  );
 };
 
 export default VendorsScreen;
