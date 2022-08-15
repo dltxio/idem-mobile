6f260f329f604a0ccee427fd4284b6fe91d7a1b0
JasminFarhi <jasmin.farhi@dltx.io>
2022-08-15 13:30:44 +1000

    Added linking to email

--- app/src/navigation/screens/SettingsScreen.tsx
+++ app/src/navigation/screens/SettingsScreen.tsx
@@ -4,19 +4,13 @@ import { View, Dimensions, StyleSheet, Linking } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 const SettingsScreen: React.FC = () => {
-  const supportURL = "https://idem.com.au/";
-
-  const goToIdem = useCallback(async () => {
-    const supported = await Linking.canOpenURL(supportURL);
-    if (supported) {
-      await Linking.openURL(supportURL);
-    }
-  }, []);
-
   return (
     <View style={commonStyles.screenContent}>
       <View style={styles.documentsList}>
-        <AntDesign.Button name="right" onPress={goToIdem}>
+        <AntDesign.Button
+          name="right"
+          onPress={() => Linking.openURL("mailto:support@example.com")}
+        >
           Contact Support
         </AntDesign.Button>
       </View>
