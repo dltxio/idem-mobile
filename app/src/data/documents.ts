9c7b1f3a6ab0a5d6f51a4357a0731bcaf2cf0c32
Robert Bartlett <amagon270@gmail.com>
2022-09-21 13:48:30 +1000

    uncommented document types
    Signed-off-by: Robert Bartlett <amagon270@gmail.com>

--- app/src/data/documents.ts
+++ app/src/data/documents.ts
@@ -1,33 +1,33 @@
 import { Document } from "../types/document";
 
 const documents: Document[] = [
-  // {
-  //   type: "passport",
-  //   title: "Passport",
-  //   fields: [
-  //     {
-  //       title: "Passport Number",
-  //       type: "number"
-  //     },
-  //     {
-  //       title: "First Name",
-  //       type: "string"
-  //     },
-  //     {
-  //       title: "Middle Name",
-  //       type: "string",
-  //       optional: true
-  //     },
-  //     {
-  //       title: "Last Name",
-  //       type: "string"
-  //     },
-  //     {
-  //       title: "Date Of Birth",
-  //       type: "date"
-  //     }
-  //   ]
-  // },
+  {
+    type: "passport",
+    title: "Passport",
+    fields: [
+      {
+        title: "Passport Number",
+        type: "number"
+      },
+      {
+        title: "First Name",
+        type: "string"
+      },
+      {
+        title: "Middle Name",
+        type: "string",
+        optional: true
+      },
+      {
+        title: "Last Name",
+        type: "string"
+      },
+      {
+        title: "Date Of Birth",
+        type: "date"
+      }
+    ]
+  },
   {
     type: "drivers-licence",
     title: "Driver Licence",
@@ -59,22 +59,22 @@ const documents: Document[] = [
       }
     ]
   },
-  // {
-  //   type: "birth-certificate",
-  //   title: "Birth Certificate"
-  // },
-  // {
-  //   type: "bank-statement",
-  //   title: "Bank Statement"
-  // },
-  // {
-  //   type: "rates-notice",
-  //   title: "Rates Notice"
-  // },
-  // {
-  //   type: "utility-account",
-  //   title: "Utility Account"
-  // },
+  {
+    type: "birth-certificate",
+    title: "Birth Certificate"
+  },
+  {
+    type: "bank-statement",
+    title: "Bank Statement"
+  },
+  {
+    type: "rates-notice",
+    title: "Rates Notice"
+  },
+  {
+    type: "utility-account",
+    title: "Utility Account"
+  },
   {
     type: "medicare-card",
     title: "Medicare Card",
